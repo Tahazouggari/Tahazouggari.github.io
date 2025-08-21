59a76a41b0629595178fa4364e479308c2c775a0
zouggari <zouggaritaha6@gmail.com>
2025-08-21 15:41:26 +0200

    Deploy static site

--- _next/static/chunks/app/badges/page-70f0de9ba704b9bd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[202,327],{9370:function(n,_,s){Promise.resolve().then(s.t.bind(s,3542,23))},3542:function(n){n.exports={style:{fontFamily:"'__CalSans_399708', '__CalSans_Fallback_399708'"},className:"__className_399708"}}},function(n){n.O(0,[971,596,744],function(){return n(n.s=9370)}),_N_E=n.O()}]);
